ec36376f4fdd85477c936d571f8cbe4494e470c9
akakiy <ivachkevich63@gmail.com>
2022-04-15 11:37:50 +0300

    run code

---
+++ src/main/java/HelloGoogle.java
@@ -0,0 +1,32 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.openqa.selenium.By.cssSelector;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class HelloGoogle {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        try {
+            driver.manage().window().maximize();
+            driver.get("https://google.com/ncr");
+            driver.findElement(By.name("q")).sendKeys("yacht" + Keys.ENTER);
+            driver.findElement(cssSelector("a[data-hveid='CAIQAw']")).click();
+            File f = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(f, new File("src/main/resources/screenshot01.png"));
+            WebElement firstResult = wait.until(presenceOfElementLocated(cssSelector("h3")));
+            System.out.println(firstResult.getAttribute("textContent"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+}
